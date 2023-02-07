dddd3af88ad6b1dc7d551a1095907ad8fe37e90b
slickcodes <hartpaulisimo@gmail.com>
2023-02-07 13:48:54 +0100

    add external type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,24 @@
 
+interface ObjectToHTML {
+    render: HTMLElement
+    unrender: HTMLObject
+}
 
+interface o2h {
+    render: Function
+    undoRender: Function
+}
+interface HTMLAttribute {
+    [key: string]: string
+}
 
-// custom HTMLElement type
-export type o2hElement = HTMLElement
+interface HTMLObject {
+    tagName?: string;
+    type?: "element" | "text" | "comment";
+    text?: string;
+    styles?: { [key: string]: string | number };
+    classes?: string | string[];
+    attributes?: HTMLAttribute[];
+    events?: { [key: string]: Function }[];
+    childNodes?: HTMLObject[];
+}
